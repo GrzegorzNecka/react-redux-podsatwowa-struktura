aab9ec0e2d57b2f905e72ee0687bea1dcc603344
GrzegorzNecka <strzypior@gmail.com>
2021-07-13 15:48:25 +0200

    1 file modified, 1 added

--- src/Root.js
+++ src/Root.js
@@ -1,21 +1,31 @@
-import React from "react";
-import { createStore } from "redux";
-import App from "./App";
+import React from 'react';
+import { createStore } from 'redux';
+import App from './App';
 
+// -------------------- STATE ------------------------- //
 const initialMovies = {
-  movies: ["Rambo III", "Hakerzy", "Matrix"]
+  movies: ['Rambo III', 'Hakerzy', 'Matrix']
 };
-
-function moviesReducer(state = initialMovies, action) {
+// -------------------- REDUCER ------------------------- //
+function initialReducer(state = initialMovies, action) {
   switch (action.type) {
+    case 'ADD':
+      return {
+        ...state,
+        movies: [...state.movies, action.movie]
+      };
+
+    case 'RESET':
+      return { ...state, movies: [] };
     default:
       return state;
   }
 }
 
-const store = createStore(moviesReducer);
+// -------------------- STORE ------------------------- //
+const store = createStore(initialReducer);
 
-window.store = store;
+console.log(store);
 
 const Root = () => {
   return <App />;
---
+++ src/Root_1.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStore } from "redux";
+import App from "./App";
+
+const initialMovies = {
+  movies: ["Rambo III", "Hakerzy", "Matrix"]
+};
+
+function moviesReducer(state = initialMovies, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = createStore(moviesReducer);
+
+window.store = store;
+
+const Root = () => {
+  return <App />;
+};
+
+export default Root;
